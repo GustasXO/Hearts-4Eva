55fd7e6400a27dcaf4aeeae25b285c6765eaeb47
GustasXO <96792559+GustasXO@users.noreply.github.com>
2022-04-21 18:23:14 +0200

    Added A Couple More Features

--- main.ts
+++ main.ts
@@ -1,11 +1,35 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        `)
+})
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showString("❤")
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showString("ღ")
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("<3")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.SmallHeart)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . . . # .
+        . # . . #
+        # . . # .
+        . # . . #
+        . . . # .
+        `)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showString("♡")
